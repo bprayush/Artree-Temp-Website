ec9fccc30b9bc9071e3b153246bfce93c1645b5e
bprayush <bprayush@yahoo.com>
2018-10-25 23:38:55 +0545

    revised website

---
+++ js/routes.js
@@ -0,0 +1,178 @@
+$(document).ready(function(){
+
+    $("#logo-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "index.html";
+    });
+
+    $("#about-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "about-us.html#artree";
+    });
+
+    $("#team-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "about-us.html#team";
+    });
+
+    $("#2018-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "projects.html#2018";
+    });
+
+    $("#2017-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "projects.html#2017";
+    });
+
+    $("#2016-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "projects.html#2016";
+    });
+
+    $("#2015-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "projects.html#2015";
+    });
+
+    $("#contact-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "index.html#contact-us";
+    });
+
+    $(".open-studio-2017-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "open-studio-2017.html";
+    });
+
+    $(".kathmandu-triennale-2017-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "kathmandu-triennale-2017.html";
+    });
+
+    $(".young-subcontinent-2017-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "young-subcontinent-project-2017.html";
+    });
+
+    $(".asia-young-36-2016-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "asia-young-36-2016.html";
+    });
+
+    $(".dhaka-art-summit-2016-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "dhaka-art-summit-2016.html";
+    });
+
+    $(".india-art-fair-2016-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "india-art-fair-2016.html";
+    });
+
+    $(".iohac-2016-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "iohac-2016.html";
+    });
+
+    $(".parallel-realities-2016-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "parallel-realities-2016.html";
+    });
+
+    $(".yinchuan-2016-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "yinchuan-biennial-of-contemporary-art-2016.html";
+    });
+
+    $(".himalayan-art-festival-2018-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "himalayan-art-festival-2018.html";
+    });
+
+    $(".amalgam-2018-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "amalgam-2018.html";
+    });
+
+    $(".52-artist-2018-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "52-artist-2018.html";
+    });
+
+    $(".dhaka-art-summit-2018-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "dhaka-art-summit-2018.html";
+    });
+
+    $(".health-transformed-by-art-2018-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "health-transformed-by-art-2018.html";
+    });
+
+    $(".hitman-gurung-bio-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "hitman-gurung.html";
+    });
+
+    $(".sheelasha-rajbhandari-bio-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "sheelasha-rajbhandari.html";
+    });
+
+    $(".mekh-limbu-bio-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "mekh-limbu.html";
+    });
+
+    $(".subas-tamang-bio-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "subas-tamang.html";
+    });
+
+    $(".lavkant-chaudhary-bio-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "lavkant-chaudhary.html";
+    });
+
+    $(".bikash-shrestha-bio-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "bikash-shrestha.html";
+    });
+
+    $(".home-city-2017-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "home-city-2017.html";
+    });
+
+    $(".monsoon-print-making-2017-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "monsoon-printmaking-exhibition-2017.html";
+    });
+
+    $(".disensus-2017-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "disensus-2017.html";
+    });
+
+    $(".serendipity-art-festival-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "serendipity-art-festival-2016.html";
+    });
+
+    $(".culture-of-silence-p1-2016-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "culture-of-silence-p1-2016.html";
+    });
+
+    $(".culture-of-silence-p2-2016-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "culture-of-silence-p2-2016.html";
+    });
+
+    $(".v19-group-art-2016-btn").click(function(e){
+        e.preventDefault();
+        window.location.href = "v19-group-art-exhibition-2016.html";
+    });
+
+});
